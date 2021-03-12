ec416c9edf1379988ce9616c059b93ac20247259
karel-vdm <karel.mobiledev@gmail.com>
2021-03-12 14:03:17 +0200

    Created DDD folder structure

--- MovieApp/app/src/main/java/com/karel/movieapp/presentation/MainActivity.kt
+++ MovieApp/app/src/main/java/com/karel/movieapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.karel.movieapp
+package com.karel.movieapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.karel.movieapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
